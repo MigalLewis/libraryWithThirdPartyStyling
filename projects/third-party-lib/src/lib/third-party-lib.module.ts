ef62c1fc1b18eb25dc248fef2b18cf7932cfb1c9
Migal <migallewis@gmail.com>
2020-08-28 11:50:56 +0200

    Added header component

--- projects/third-party-lib/src/lib/third-party-lib.module.ts
+++ projects/third-party-lib/src/lib/third-party-lib.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ThirdPartyLibComponent } from './third-party-lib.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [ThirdPartyLibComponent],
+  declarations: [ThirdPartyLibComponent, HeaderComponent],
   imports: [
   ],
-  exports: [ThirdPartyLibComponent]
+  exports: [
+    ThirdPartyLibComponent,
+    HeaderComponent
+  ]
 })
 export class ThirdPartyLibModule { }
