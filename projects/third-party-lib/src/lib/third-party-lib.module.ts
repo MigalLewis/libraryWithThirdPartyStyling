a67b5c1604900e820534806ed46ae877f535739c
Migal <migallewis@gmail.com>
2020-08-28 14:10:13 +0200

    Added angular material component

--- projects/third-party-lib/src/lib/third-party-lib.module.ts
+++ projects/third-party-lib/src/lib/third-party-lib.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { ThirdPartyLibComponent } from './third-party-lib.component';
 import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @NgModule({
   declarations: [ThirdPartyLibComponent, HeaderComponent],
   imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     ThirdPartyLibComponent,
