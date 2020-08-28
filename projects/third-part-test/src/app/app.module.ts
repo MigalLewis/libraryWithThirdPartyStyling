9a00479c48a6fc9c05c175b75e9029cc0e7d4807
Migal <migallewis@gmail.com>
2020-08-28 12:07:10 +0200

    Added third party library @angular/material

--- projects/third-part-test/src/app/app.module.ts
+++ projects/third-part-test/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThirdPartyLibModule } from 'third-party-lib';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ThirdPartyLibModule } from 'third-party-lib';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ThirdPartyLibModule
+    ThirdPartyLibModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
